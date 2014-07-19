51af69ceb22e990631b6de49b8e0a92f05e6e325
Tyler Palsulich <tpalsulich@gmail.com>
2014-07-19 12:01:30 -0700

    Use the stop ID as URL id, instead of db index.

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,10 @@
 class Stop < ActiveRecord::Base
+    validates_uniqueness_of :stop_id
+    def to_param
+        stop_id
+    end
+
+    def self.find(input)
+        find_by stop_id: input
+    end
 end
