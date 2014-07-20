c77345f1f5c075da80a30ce2769fb1df765f3bfe
Tyler Palsulich <tpalsulich@gmail.com>
2014-07-20 00:03:40 -0700

    Add delete and update abilities for CRUD.

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -15,11 +15,25 @@ class StopsController < ApplicationController
         @stop = Stop.find(params[:id])
     end
 
+    def update 
+      @stop = Stop.find(params[:id])
+     
+      if @stop.update(stop_params)
+        redirect_to stop_path(@stop.stop_id)
+      else
+        render 'edit'
+      end
+    end
+
     def destroy
         @stop = Stop.find(params[:id])
         @stop.destroy
 
         redirect_to stops_path
+    end
+
+    def edit
+        @stop = Stop.find(params[:id])
     end
 
     private
