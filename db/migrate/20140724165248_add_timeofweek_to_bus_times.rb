f419a8236e6c7d755a8f3f6527c16a922cea68d6
Tyler Palsulich <tpalsulich@gmail.com>
2014-07-25 09:52:56 -0700

    Remove bus_times references.

--- db/migrate/20140724165248_add_timeofweek_to_bus_times.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeofweekToBusTimes < ActiveRecord::Migration
-  def change
-    add_column :bus_times, :timeofweek, :string
-  end
-end
--- db/migrate/20140724173112_add_stopid_to_bus_times.rb
+++
@@ -1,5 +0,0 @@
-class AddStopidToBusTimes < ActiveRecord::Migration
-  def change
-    add_column :bus_times, :stop_id, :integer
-  end
-end
